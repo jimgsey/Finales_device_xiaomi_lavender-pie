1d4efe71dd690034826f9ac0c3ae051d1499828f
Bladeyy <getsuga369@gmail.com>
2019-06-09 19:18:17 +0530

    lavender: Build extra vndk libsx

--- device.mk
+++ device.mk
@@ -403,7 +403,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
-
+    
 # VNDK
 PRODUCT_PACKAGES += \
 	libstdc++.vendor \
