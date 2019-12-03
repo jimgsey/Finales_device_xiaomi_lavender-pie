ec5323ddaa9173daeba0095e364b4308a7992edd
PikaPikaFreed <pikapikafreed@gmail.com>
2019-12-03 10:42:32 +0800

    lavender: use Snap2

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Snap
+    Snap2
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
