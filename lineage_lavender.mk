8f49cf5277df0e951f48b2ee8b04b5ba9cfc8ab2
PikaPikaFreed <pikapikafreed@gmail.com>
2019-12-03 10:12:16 +0800

    lavender: declare TARGET_BOOT_ANIMATION_RES

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_lavender
